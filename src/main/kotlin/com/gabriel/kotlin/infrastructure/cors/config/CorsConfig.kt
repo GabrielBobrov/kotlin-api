7bdc7381aae795d96dc9b4691a1fd37401fbbf66
Gabriel Bobrov <bielbobrov@hotmail.com>
2025-01-09 15:30:52 -0300

    feat: add cors config

---
+++ src/main/kotlin/com/gabriel/kotlin/infrastructure/cors/config/CorsConfig.kt
@@ -0,0 +1,23 @@
+package com.gabriel.kotlin.infrastructure.cors.config
+
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+open class CorsConfig {
+
+    @Bean
+    open fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+            }
+        }
+    }
+}
